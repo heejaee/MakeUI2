3529e424e78fcb681e292c4c1a02fc1b58b520d8
hee jae <hjhjhjhj3770@naver.com>
2021-10-07 15:22:07 +0900

    First

--- app/src/main/java/com/example/makeui2/MainActivity.java
+++ app/src/main/java/com/example/makeui2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.makeui2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.alarm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this , AlarmActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
     }
 }
